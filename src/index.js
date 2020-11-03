870d1fdf4e457387bef45e975d1af2277d0910ba
yeongseok.kim <yeongseok.kim@bespinglobal.com>
2020-11-03 18:09:30 +0900

    image file-loader added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './coin.png';
 
 function component() {
     const element = document.createElement('div');
@@ -7,6 +8,11 @@ function component() {
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
 
     return element;
 }
